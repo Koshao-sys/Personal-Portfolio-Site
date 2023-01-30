9a4470d5d1d494b6fc8d037deba656e4978e0253
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-01-30 22:04:39 +0300

    Added menu visibility on mobile view

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 const toggleNav = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav');
+const closeMenu = document.querySelector('.close-menu');
+const closeLinks = document.querySelectorAll('.nav a');
+const closeLink = '';
 
-toggleNav.addEventListener('click', function() {
+toggleNav.addEventListener('click', () => {
   nav.classList.toggle('open');
-  
+  closeMenu.style.display = nav.classList.contains('open') ? 'block' : 'none';
+  toggleNav.style.display = nav.classList.contains('open') ? 'none' : 'block';
+});
+
+closeMenu.addEventListener('click', () => {
+  nav.classList.remove('open');
+  closeMenu.style.display = 'none';
+  toggleNav.style.display = 'block';
+});
+
+closeLinks.forEach(closeLink, () => {
+  closeLink.addEventListener('click', (e) => {
+    nav.classList.remove('open');
+    closeMenu.style.display = 'none';
+    toggleNav.style.display = 'block';
+    e.preventDefault();
+    const targetId = e.target.getAttribute('href');
+    const target = document.querySelector(targetId);
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
 });
